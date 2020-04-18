f372fc23f33506a2710803509621875bfa4040a4
Diego Orejuela <dfrodriguezor@gmail.com>
2020-04-18 16:17:55 +0000

    Improvement for review with pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
 
     if not validate_boxes(boxes):
@@ -9,23 +10,27 @@ def canUnlockAll(boxes):
     check_boxes_queue = boxes[index]
     unlocked_boxes = [index]
 
-    while there_are_boxes_to_review(check_boxes_queue, index):
-        box_index = check_boxes_queue[index]
-        if not box_index in unlocked_boxes:
-            box = boxes[box_index]
-            for box_key in box:
-                check_boxes_queue.append(box_key) if not box_key in check_boxes_queue else None
-            unlocked_boxes.append(box_index)
+    while index_inside_range(check_boxes_queue, index):
+        if index_inside_range(boxes, index):
+            box_index = check_boxes_queue[index]
+            if box_index not in unlocked_boxes:
+                box = boxes[box_index]
+                for box_key in box:
+                    if box_key not in check_boxes_queue:
+                        check_boxes_queue.append(box_key)
+                unlocked_boxes.append(box_index)
         index += 1
 
     return True if all_boxes_unlocked(boxes, unlocked_boxes) else False
 
 
 def validate_boxes(boxes):
-    return boxes != None and boxes != [] and boxes[0] != []
+    return boxes is not None and boxes != [] and boxes[0] != []
 
-def there_are_boxes_to_review(list, index):
+
+def index_inside_range(list, index):
     return index < len(list)
+
 
 def all_boxes_unlocked(origin_boxes, unlocked_boxes):
     return len(origin_boxes) == len(unlocked_boxes)
