73dcb2aa1ca138bdecbe99613d78806399d87cbf
Diego Orejuela <dfrodriguezor@gmail.com>
2020-04-21 18:39:13 +0000

    delete conditions not necessary

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,16 +7,13 @@ def canUnlockAll(boxes):
     boxes. This method that determines if all the boxes can be opened.
 
     Return True if all boxes can be opened, else return False"""
-    if boxes == [] or boxes == [[]]:
-        return True
-
     list_comparation = [0]
+
     for box in range(len(boxes)):
         if boxes[box] == [] and box not in list_comparation:
             list_comparation.append(box)
         for key in boxes[box]:
             if key != box and key < len(boxes) and key not in list_comparation:
                 list_comparation.append(key)
-    # print()
-    # print(list_comparation)
+
     return True if len(list_comparation) == len(boxes) else False
