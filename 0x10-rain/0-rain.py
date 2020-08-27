666b2c4f02c6c55cbe663fcd6987765f614ace61
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-27 03:46:23 +0000

    rain 3

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -26,42 +26,35 @@ def rain(walls):
 
     for idx, wall in enumerate(walls):
 
-        # if there is a previous wall and a new one to evaluate
         if wall != 0 and previous_wall != 0:
 
-            # the current wall is greater than the previous wall
             if wall >= previous_wall:
                 retained += (previous_wall * len(spaces)) - sum(spaces)
                 spaces = []
                 previous_wall = wall
-                if mayor <= wall:
-                    mayor = 0
 
-            # the current wall is less than the previous wall
             if wall < previous_wall:
 
-                # check if there is a bigger wall in front
                 if mayor == 0:
                     mayor = max(walls[idx:])
 
-                    # there are no more walls to check
                     if mayor == 0:
                         retained += (wall * len(spaces)) - sum(spaces)
                         break
 
-                # current wall is greater than or equal to the following walls
                 if wall >= mayor:
                     retained += (wall * len(spaces)) - sum(spaces)
                     mayor = 0
                     spaces = []
                     previous_wall = wall
 
-                # later there are walls bigger than the current, save the wall
                 if mayor > wall:
                     spaces.append(wall)
 
-        # found the first wall
         if wall != 0 and previous_wall == 0:
             previous_wall = wall
+
+        if wall == 0 and previous_wall != 0:
+            spaces.append(wall)
 
     return retained
